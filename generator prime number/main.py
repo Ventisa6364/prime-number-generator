3104caddf9473f1388570ea7be1d70841c48259b
Ventisa6364 <cherkgosa@icloud.com>
2025-08-29 18:45:19 +0300

    Версия со всеми правками и красотой

--- generator prime number/main.py
+++ generator prime number/main.py
@@ -26,7 +26,7 @@ def all_prime_to_n(n):
 
 
 def all_prime_from_a_to_b(a, b):
-    if b < 2:
+    if b < 2 or a > b:
         return []
 
     prime_list = []
@@ -52,16 +52,6 @@ def x_first_prime_number(x):
     return list_prime_num
 
 
-def sum_nums_in_list(list_prime_num):
-
-    sum_nums = 0
-
-    for i in list_prime_num:
-        sum_nums += i
-
-    return sum_nums
-
-
 def action_selection(act):
 
     match int(act):
@@ -71,26 +61,35 @@ def action_selection(act):
                     "Введите число, до которого включительно будут проверены все числа: "
                 )
             )
-            print(all_prime_to_n(num))
+            print("Вот список простых чисел от 0 до N:", all_prime_to_n(num))
 
         case 2:
             n = int(input("Введите количество чисел, которые необходимо найти: "))
-            print(x_first_prime_number(n))
+            result = x_first_prime_number(n)
+            print("Вот список N первых простых чисел:", result)
+            print("Всего чисел в списке:", len(result))
 
         case 3:
             a, b = int(input("Введите А:")), int(input("Введите В:"))
-            print(all_prime_from_a_to_b(a, b))
+            result = all_prime_from_a_to_b(a, b)
+            print(f"Вот список простых чисел от {a} до {b}: {result}")
+            print("Всего чисел в списке:", len(result))
 
         case 4:
-            num = int(input("Введите числло для проверки: "))
-            print(is_prime(num))
+            num = int(input("Введите число для проверки: "))
+            if is_prime(num):
+                print(f"Число {num} является простым")
+            else:
+                print(f"Число {num} не является простым")
 
         case 5:
             a, b = int(input("Введите стартовую точку диапазона: ")), int(
                 input("Введите конечную точку диапазона: ")
             )
             list_num = all_prime_from_a_to_b(a, b)
-            print(sum_nums_in_list(list_num))
+            print(
+                f"Сумма простых чисел в диапазоне от {a} до {b}: {sum(list_num)}"
+            )
 
         case _:
             act = input("Ошибка, введите номер действия заново: ")
@@ -101,12 +100,16 @@ print(
     """ Какое действие Вы хотели бы выполнить?
       1. Найти все простые числа до N
       2. Найти N простых чисел
-      3. Найти все простые числа от A до N
+      3. Найти все простые числа от A до B
       4. Узнать, является ли конкретное число простым
       5. Вывести сумму простых чисел в диапазоне
+      0. Выйти из программы
 """
 )
 
 act = input("Введите номер действия: ")
 
-action_selection(act)
+if act == "0":
+        print("Выход из программы.")
+else:
+    action_selection(act)
