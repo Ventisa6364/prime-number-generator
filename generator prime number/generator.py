faa28336a8e1e5a5fd0e59ef8b65df0aeda86121
Ventisa6364 <cherkgosa@icloud.com>
2025-09-07 07:11:09 +0300

    изменения в функции нахождения чисел в промежутке

--- generator prime number/generator.py
+++ generator prime number/generator.py
@@ -7,20 +7,18 @@ def is_prime(num):  # проверка числа на простоту
     return True
 
 
-def prime_list(a, b):  # все простые числа от a до b включительно (объединено с числами до n)
+def get_primes_in_range(a, b): # поиск простых чисел в промежутке
     if a > b:
         a, b = b, a
-        if a < 2:
-            a = 2
-    elif b == a:
+    if b < 2:
         return []
-    else:
-        if b < 2:
-            return []
+    if a < 2:
+        a = 2
+
     prime_list = []
     for i in range(a, b + 1):
         if is_prime(i):
-           prime_list.append(i)
+            prime_list.append(i)
     return prime_list
 
 
@@ -41,7 +39,7 @@ def action_selection(act):  # выбор действия
     match int(act):
         case 1:
             num = int(input("\nВведите число, числа до которого будут проверены: ")) # все простые числа до n включительно
-            primes = prime_list(0, num)
+            primes = get_primes_in_range(0, num)
             print(f"\nВот список простых чисел от 0 до {num}:\n")
             print(', '.join(map(str, primes)))
             print(f"\nВсего простых чисел: {len(primes)}\n")
@@ -56,7 +54,7 @@ def action_selection(act):  # выбор действия
         case 3:
             a = int(input("\nВведите А: ")) # все простые числа от a до b включительно
             b = int(input("Введите В: "))
-            result = prime_list(a, b)
+            result = get_primes_in_range(a, b)
             print(f"\nВот список простых чисел от {a} до {b}:\n")
             print(', '.join(map(str, result)))
             print(f"\nВсего чисел в списке: {len(result)}\n")
@@ -71,7 +69,7 @@ def action_selection(act):  # выбор действия
         case 5:
             a = int(input("\nВведите стартовую точку диапазона: ")) # сумма простых чисел в диапазоне от a до b включительно
             b = int(input("Введите конечную точку диапазона: "))
-            list_num = prime_list(a, b)
+            list_num = get_primes_in_range(a, b)
             print(f"\nСумма простых чисел в диапазоне от {a} до {b}: {sum(list_num)}\n")
 
         case _:
@@ -87,7 +85,7 @@ def action_selection(act):  # выбор действия
         exit()
 
 
-while True:  #цикл с выводом меню
+while True:  # цикл с выводом меню
     print("""
 ==================================================
 Какое действие Вы хотели бы выполнить?
