1b2a2db1aceaed5281cc5fa5ae2ce2371e9971d9
Ventisa6364 <cherkgosa@icloud.com>
2025-09-07 12:31:21 +0300

    убрали рекурсию и добавили проверку ввода

--- generator prime number/generator.py
+++ generator prime number/generator.py
@@ -34,55 +34,71 @@ def x_first_prime_number(x):  # первые x простых чисел
     return list_prime_num
 
 
-def action_selection(act):  # выбор действия
-    print("\n" + "-" * 50)
-    match int(act):
-        case 1:
-            num = int(input("\nВведите число, числа до которого будут проверены: ")) # все простые числа до n включительно
-            primes = get_primes_in_range(0, num)
-            print(f"\nВот список простых чисел от 0 до {num}:\n")
-            print(', '.join(map(str, primes)))
-            print(f"\nВсего простых чисел: {len(primes)}\n")
+def check_is_digit(num):
+    while not num.isdigit():
+        num = input("Ошибка, введите число заново: ")
+    return int(num)
 
-        case 2:
-            n = int(input("\nВведите количество чисел, которые необходимо найти: ")) # первые n простых чисел
-            result = x_first_prime_number(n)
-            print(f"\nВот список {n} первых простых чисел:\n")
-            print(', '.join(map(str, result)))
-            print(f"\nВсего чисел в списке: {len(result)}\n")
 
-        case 3:
-            a = int(input("\nВведите А: ")) # все простые числа от a до b включительно
-            b = int(input("Введите В: "))
-            result = get_primes_in_range(a, b)
-            print(f"\nВот список простых чисел от {a} до {b}:\n")
-            print(', '.join(map(str, result)))
-            print(f"\nВсего чисел в списке: {len(result)}\n")
+def action_selection():  # выбор действия
+    while True:
+        act = input("Введите номер действия: ")
+        print("\n" + "-" * 50)
 
-        case 4:
-            num = int(input("\nВведите число для проверки: ")) # проверка числа на простоту
-            if is_prime(num):
-                print(f"\nДа, число {num} является простым\n")
+        match act:
+            case "1":
+                num = input("\nВведите число, числа до которого будут проверены: ")
+                num = check_is_digit(num)
+                primes = get_primes_in_range(0, num)
+                print(f"\nВот список простых чисел от 0 до {num}: {', '.join(map(str, primes))}")
+                print(f"\nВсего простых чисел: {len(primes)}\n")
+
+            case "2":
+                n = input("\nВведите количество чисел, которые необходимо найти: ")
+                n = check_is_digit(n)
+                result = x_first_prime_number(n)
+                print(f"\nВот список {n} первых простых чисел: {', '.join(map(str, result))} \n")
+
+            case "3":
+                a = check_is_digit(input("\nВведите А: "))
+                b = check_is_digit(input("Введите В: "))
+                result = get_primes_in_range(a, b)
+                print(f"\nВот список простых чисел от {a} до {b}: {', '.join(map(str, result))}")
+                print(f"\nВсего чисел в списке: {len(result)}\n")
+
+            case "4":
+                num = check_is_digit(input("\nВведите число для проверки: "))
+                if is_prime(num):
+                    print(f"\nДа, число {num} является простым\n")
+                else:
+                    print(f"\nНет, число {num} не является простым\n")
+
+            case "5":
+                a = check_is_digit(input("\nВведите стартовую точку диапазона: "))
+                b = check_is_digit(input("Введите конечную точку диапазона: "))
+                print(f"\nСумма простых чисел в диапазоне от {a} до {b}: {sum(get_primes_in_range(a, b))}\n")
+
+            case "0":
+                print("\nВыход из программы. До свидания!\n")
+                exit()
+
+            case _:
+                print("\nОшибка, введите номер действия заново.\n")
+                continue   # возвращаемся в начало цикла и ждём правильный ввод
+
+        # если дошли сюда → действие выполнено
+        print("-" * 50 + "\n")
+        print("Вернемся в главное меню или хотите выйти из программы?")
+
+        while True:
+            choice = input("\nВведите номер действия (1 - меню, 0 - выход): ")
+            if choice == "0":
+                print("\nВыход из программы. До свидания!\n")
+                exit()
+            elif choice == "1":
+                return   # возвращаемся в главное меню
             else:
-                print(f"\nНет, число {num} не является простым\n")
-
-        case 5:
-            a = int(input("\nВведите стартовую точку диапазона: ")) # сумма простых чисел в диапазоне от a до b включительно
-            b = int(input("Введите конечную точку диапазона: "))
-            list_num = get_primes_in_range(a, b)
-            print(f"\nСумма простых чисел в диапазоне от {a} до {b}: {sum(list_num)}\n")
-
-        case _:
-            act = input("\nОшибка, введите номер действия заново: ") 
-            action_selection(act)
-
-    print("-" * 50 + "\n")
-    print("Вернемся в главное меню или хотите выйти из программы?")
-
-    choice = input("\nВведите номер действия (1 - меню, 0 - выход): ")
-    if choice == "0":
-        print("\nВыход из программы.\n")
-        exit()
+                print("\nОшибка, введите номер действия заново.")
 
 
 while True:  # цикл с выводом меню
@@ -98,10 +114,4 @@ while True:  # цикл с выводом меню
 ==================================================
 """)
 
-    act = input("Введите номер действия: ")
-
-    if act == "0":
-        print("\nВыход из программы.")
-        break
-
-    action_selection(act)
+    action_selection()
