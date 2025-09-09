49ec427f7980d1d19d9350ea17225f4177c99a9f
Ventisa6364 <cherkgosa@icloud.com>
2025-09-09 18:56:05 +0300

    отформатированный код согласно PEP-8

--- generator prime number/generator.py
+++ generator prime number/generator.py
@@ -7,7 +7,7 @@ def is_prime(num):  # проверка числа на простоту
     return True
 
 
-def get_primes_in_range(a, b): # поиск простых чисел в промежутке
+def get_primes_in_range(a, b):  # поиск простых чисел в промежутке
     if a > b:
         a, b = b, a
     if b < 2:
@@ -34,8 +34,8 @@ def x_first_prime_number(x):  # первые x простых чисел
     return list_prime_num
 
 
-def check_is_digit(string_output): # проверка ввода на число
-    print(string_output, end='')
+def check_is_digit(string_output):  # проверка ввода на число
+    print(string_output, end="")
     num = input("")
     while not num.isdigit():
         num = input("Ошибка, введите число заново: ")
@@ -49,21 +49,31 @@ def action_selection():  # выбор действия
 
         match act:
             case "1":
-                num = check_is_digit("\nВведите число, числа до которого будут проверены: ")
+                num = check_is_digit(
+                    "\nВведите число, числа до которого будут проверены: "
+                )
                 primes = get_primes_in_range(0, num)
-                print(f"\nВот список простых чисел от 0 до {num}: {', '.join(map(str, primes))}")
+                print(
+                    f"\nВот список простых чисел от 0 до {num}: {', '.join(map(str, primes))}"
+                )
                 print(f"\nВсего простых чисел: {len(primes)}\n")
 
             case "2":
-                n = check_is_digit("\nВведите количество чисел, которые необходимо найти: ")
+                n = check_is_digit(
+                    "\nВведите количество чисел, которые необходимо найти: "
+                )
                 result = x_first_prime_number(n)
-                print(f"\nВот список {n} первых простых чисел: {', '.join(map(str, result))} \n")
+                print(
+                    f"\nВот список {n} первых простых чисел: {', '.join(map(str, result))} \n"
+                )
 
             case "3":
                 a = check_is_digit("\nВведите А: ")
                 b = check_is_digit("Введите В: ")
                 result = get_primes_in_range(a, b)
-                print(f"\nВот список простых чисел от {a} до {b}: {', '.join(map(str, result))}")
+                print(
+                    f"\nВот список простых чисел от {a} до {b}: {', '.join(map(str, result))}"
+                )
                 print(f"\nВсего чисел в списке: {len(result)}\n")
 
             case "4":
@@ -76,7 +86,9 @@ def action_selection():  # выбор действия
             case "5":
                 a = check_is_digit("\nВведите стартовую точку диапазона: ")
                 b = check_is_digit("Введите конечную точку диапазона: ")
-                print(f"\nСумма простых чисел в диапазоне от {a} до {b}: {sum(get_primes_in_range(a, b))}\n")
+                print(
+                    f"\nСумма простых чисел в диапазоне от {a} до {b}: {sum(get_primes_in_range(a, b))}\n"
+                )
 
             case "0":
                 print("\nВыход из программы. До свидания!\n")
@@ -84,7 +96,7 @@ def action_selection():  # выбор действия
 
             case _:
                 print("\nОшибка, введите номер действия заново.\n")
-                continue   # возвращаемся в начало цикла и ждём правильный ввод
+                continue  # возвращаемся в начало цикла и ждём правильный ввод
 
         # если дошли сюда → действие выполнено
         print("-" * 50 + "\n")
@@ -96,13 +108,14 @@ def action_selection():  # выбор действия
                 print("\nВыход из программы. До свидания!\n")
                 exit()
             elif choice == "1":
-                return   # возвращаемся в главное меню
+                return  # возвращаемся в главное меню
             else:
                 print("\nОшибка, введите номер действия заново.")
 
 
 while True:  # цикл с выводом меню
-    print("""
+    print(
+        """
 ==================================================
 Какое действие Вы хотели бы выполнить?
   1. Найти все простые числа до N
@@ -112,6 +125,7 @@ while True:  # цикл с выводом меню
   5. Вывести сумму простых чисел в диапазоне
   0. Выйти из программы
 ==================================================
-""")
+"""
+    )
 
     action_selection()
